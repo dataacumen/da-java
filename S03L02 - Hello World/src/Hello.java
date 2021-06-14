2bc6d8e9caffa419252aafa5c3a938b65f7a5be1
Ronald White <dataacumen@users.noreply.github.com>
2021-06-14 08:31:44 -0700

    Add files via upload

---
+++ S03L02 - Hello World/src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        int value1 = 1;
+        int value2 = 2;
+        System.out.println("Hello World! Result: " + (value1 + value2));
+    }
+}
